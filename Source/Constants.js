0121a434b27e7e45b1019cfe79cc20ca5f361683
Ameer AbouHammoud <aabouhammoud@hotmail.com>
2023-01-15 01:19:04 +0300

    new components

---
+++ Source/Constants.js
@@ -0,0 +1,6 @@
+
+let cnst = {
+    cnst_user_settings_cookie : "jupiter_user_settings"
+}
+
+export default cnst
--- Source/JupiterWeather.jsx
+++ Source/JupiterWeather.jsx
@@ -1,34 +1,49 @@
 
 const { Component, useEffect, useState, useRef } = require("react");
+import { useCookies } from "react-cookie";
+import cnst from "./Constants";
 import REQUEST from "../Components/Request";
+
+function load_page_content (cookies, setCookie) {
+
+    if (!cookies[cnst.cnst_user_settings_cookie] || cookies[cnst.cnst_user_settings_cookie] == "") {
+        setCookie(cnst.cnst_user_settings_cookie, "New user")
+        return(<h1>New user</h1>);
+    } else  {
+        setCookie(cnst.cnst_user_settings_cookie, "Not a new user")
+        return(<h1>Not a new user</h1>);
+    }
+    
+}
 
 const JupiterWeather = () => {
     
-    const [HomePageContent, setHomePageContent] = useState(<h1>
-        Jupiter Weather
-    </h1>);
+    const [cookies, setCookie] = useCookies([cnst.cnst_user_settings_cookie])
 
-    const getRegions = async () => {
-        let regionsList = await REQUEST.GET(`http://dataservice.accuweather.com/locations/v1/regions/?apikey=${process.env.APIKEY}`);
-        if (regionsList.data) {
-            setHomePageContent(<table>
-                {regionsList.data.map(region => {
-                    return (
-                        <tr>
-                            <td>{region.ID}</td>
-                            <td>{region.EnglishName}</td>
-                            <td>{region.LocalizedName}</td>
-                        </tr>);
-                })}
-            </table>)
-        }
-    }
+    const [HomePageContent, setHomePageContent] = useState(load_page_content(cookies,setCookie));
+
+    //setHomePageContent(<h1>HELLO THERE</h1>);
+
+    // const getRegions = async () => {
+    //     let regionsList = await REQUEST.GET(`http://dataservice.accuweather.com/locations/v1/regions/?apikey=${process.env.APIKEY}`);
+    //     if (regionsList.data) {
+    //         setHomePageContent(<table>
+    //             {regionsList.data.map(region => {
+    //                 return (
+    //                     <tr>
+    //                         <td>{region.ID}</td>
+    //                         <td>{region.EnglishName}</td>
+    //                         <td>{region.LocalizedName}</td>
+    //                     </tr>);
+    //             })}
+    //         </table>)
+    //     }
+    // }
 
     return (
         <>
-            {HomePageContent}
-            <br />
-            <button onClick={getRegions}><span>Get Regions</span></button>
+            { HomePageContent }
+            <button onClick={() => { setHomePageContent(load_page_content(cookies, setCookie)); }}></button>
         </>
     )
 }
